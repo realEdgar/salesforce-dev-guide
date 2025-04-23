23ea56c1aa1ae4644654d79d05dab738019e7a8a
realEdgar <ing.edgar.lopez.28@gmail.com>
2025-04-23 08:17:28 -0600

    Project Account Manager: subscribe to the channel logic added.

--- lwc/accountDetailsCard/accountDetailsCard.js
+++ lwc/accountDetailsCard/accountDetailsCard.js
@@ -1,4 +1,4 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
 import ACCOUNT_PHONE from '@salesforce/schema/Account.Phone';
@@ -8,12 +8,32 @@ import ACCOUNT_CUSTOMER_PRIORITY from '@salesforce/schema/Account.CustomerPriori
 import ACCOUNT_TYPE from '@salesforce/schema/Account.Type';
 import ACCOUNT_NUMBER from '@salesforce/schema/Account.AccountNumber';
 
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext,
+} from "lightning/messageService";
+import ACCOUNT_MANAGER_CHANNEL from '@salesforce/messageChannel/AccountManagerChannel__c';
+
 const FIELDS = [ACCOUNT_NAME, ACCOUNT_PHONE, ACCOUNT_INDUSTRY, ACCOUNT_ANNUAL_REVENUE, ACCOUNT_CUSTOMER_PRIORITY, ACCOUNT_TYPE, ACCOUNT_NUMBER];
 
 export default class AccountDetailsCard extends NavigationMixin(LightningElement) {
     objectApiName = 'Account';
     fields = FIELDS;
     @api accountId;
+    subscription;
+
+    @wire(MessageContext)
+    context;
+
+    connectedCallback(){
+        this.handleSubscription();
+    }
+
+    disconnectedCallback(){
+        this.handleUnsubscription();
+    }
 
     handleViewDetailsNavigation() {
         const pageReference = {
@@ -25,5 +45,22 @@ export default class AccountDetailsCard extends NavigationMixin(LightningElement
             }
         }
         this[NavigationMixin.Navigate](pageReference, false);
+    }
+
+    handleSubscription() {
+        this.subscription = subscribe(
+                            this.context, 
+                            ACCOUNT_MANAGER_CHANNEL, 
+                            (message) => this.handleMessage(message), 
+                            { scope: APPLICATION_SCOPE }
+                        );
+    }
+
+    handleMessage(message) {
+        this.accountId = message.recordId;
+    }
+
+    handleUnsubscription(){
+        unsubscribe(this.subscription);
     }
 }
